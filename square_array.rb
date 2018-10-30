07ec6a0f4fd603472dc77cde84c52764594cbf14
Yuki N <lunarbase@outlook.com>
2018-10-30 13:54:06 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  squared = []
+  array.each do |number|
+     squared << number ** 2
+  end
+   return squared
+end
+
+#advanced bonus
+def square_advanced(array)
+  array.collect do |number|
+     number ** 2  
+  end
 end
